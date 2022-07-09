3adc9ce1e699c15023d7228e9ce293706288e1db
EugenRascanu <eugen.rascanu88@gmail.com>
2022-07-09 17:10:57 +0200

    Email Function Adjustment

--- dentist/settings.py
+++ dentist/settings.py
@@ -122,11 +122,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # accounts.google.com/DisplayUnlockCaptcha
 # myaccount.google.com/apppasswords
 # Email Settings
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'eugen.rascanu88@gmail.com'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '1025'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 # EMAIL_USE_SSL = False 
 
 
